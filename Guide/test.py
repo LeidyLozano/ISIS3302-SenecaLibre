79a07cbbf01bfef1b62e649399f4cab6dc19e28b
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-11-17 15:36:47 -0500

    test

--- Guide/test.py
+++ Guide/test.py
@@ -1,37 +1,14 @@
-from pyomo.environ import *
+import pyomo.environ as pyo
+from highspy import Highs
 
+model = pyo.ConcreteModel()
+model.x = pyo.Var([1, 2], domain=pyo.NonNegativeReals)
+model.obj = pyo.Objective(expr=model.x[1] + model.x[2])
 
-# Create model object
-model = ConcreteModel()
+model.con1 = pyo.Constraint(expr=2 * model.x[1] + model.x[2] <= 3)
+model.con2 = pyo.Constraint(expr=model.x[1] + 3 * model.x[2] <= 5)
 
-# Declare decision variables
-model.l = Var(within=NonNegativeReals)
-model.g = Var(within=NonNegativeReals)
-
-# Declare objective function
-model.maximizeProfit = Objective(expr=200 * model.l + 300 * model.g,
-sense=maximize)
-
-# Declare constraints
-# Farmer Brown has only 100 hours
-model.LaborConstraint = Constraint(expr=3 * model.l + 2 * model.g <= 100)
-# He only has $120 for medical
-model.MedicalConstraint = Constraint(expr=2 * model.l + 4 * model.g <= 120)
-# He only has 45 acres
-model.LandConstraint = Constraint(expr=model.l + model.g <= 45)
-
-# Show all available solvers
-print(SolverFactory.__dict__["_cls"].keys())
-print("")
-
-# Test for availability of solver
-my_solver = "highs"
-if SolverFactory(my_solver).available():
-    print("Solver " + my_solver + " is available.")
-else:
-    print("Solver " + my_solver + " is not available.")
-
-optimizer = SolverFactory(my_solver)
-optimizer.solve(model, options={"threads": 4}, tee=True)
+opt = pyo.SolverFactory('highs', executable='C:/Users/camix/Documents/ProgramasExtra/HiGHS/build/bin/highs.exe')
+results = opt.solve(model)
 
 model.display()
