80e4b6b055e1e95adeca11c5a7e9e157ce4ad9d9
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-11-18 07:28:48 -0500

    Test funcional

--- Guide/test.py
+++ Guide/test.py
@@ -8,7 +8,7 @@ model.obj = pyo.Objective(expr=model.x[1] + model.x[2])
 model.con1 = pyo.Constraint(expr=2 * model.x[1] + model.x[2] <= 3)
 model.con2 = pyo.Constraint(expr=model.x[1] + 3 * model.x[2] <= 5)
 
-opt = pyo.SolverFactory('highs', executable='C:/Users/camix/Documents/ProgramasExtra/HiGHS/build/bin/highs.exe')
+opt = pyo.SolverFactory('appsi_highs')
 results = opt.solve(model)
 
 model.display()
